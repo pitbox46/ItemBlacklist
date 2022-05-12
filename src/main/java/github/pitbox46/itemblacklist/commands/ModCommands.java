40c9e96b89a8f8bcbbd38dbb0f6f96d81b06aa01
X_Niter <drumsforlife1999@yahoo.com>
2022-05-11 20:34:07 -0500

    Blacklist Item being held in hand with `/itemblacklist hand`, with chat feedback confirmation.

--- src/main/java/github/pitbox46/itemblacklist/commands/ModCommands.java
+++ src/main/java/github/pitbox46/itemblacklist/commands/ModCommands.java
@@ -12,8 +12,23 @@ public class ModCommands {
                         .then(CommandBanItem.register(dispatcher))
                         .then(CommandUnbanItem.register(dispatcher))
                         .then(CommandBanList.register(dispatcher))
+                        .then(Commands.literal("hand")
+                                .executes(context -> hand(context.getSource().getPlayerOrException(), InteractionHand.MAIN_HAND))
+                        )
         );
 
         dispatcher.register(Commands.literal("itemblacklist").redirect(cmdTut));
+    }
+
+    private static int hand(ServerPlayer player, InteractionHand hand) {
+        var stack = player.getItemInHand(hand);
+        var stackcount = stack.getCount();
+
+        JsonUtils.appendItemToJson(ItemBlacklist.BANLIST, player.getItemInHand(hand).getItem());
+        player.sendMessage(new TextComponent("Item banned: ").append(stack.getItem().getRegistryName().toString()), ChatType.CHAT, Util.NIL_UUID);
+        // Below Line Removes the item from hand when blacklisting is succesful - kept out for those who wouldn't care for it unless a config option was a thing.
+        //player.getItemInHand(hand).setCount(stackcount);
+
+        return 0;
     }
 }
