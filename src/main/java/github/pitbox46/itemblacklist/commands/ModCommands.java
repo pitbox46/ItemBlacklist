3efac4f45e1bc78baba791b4a9a170d55c1f0112
X_Niter <drumsforlife1999@yahoo.com>
2022-05-11 19:51:35 -0500

    Ban Item being held in hand with `/itemblacklist hand`, then it send chat msg to confirm and deletes it from hand.

--- src/main/java/github/pitbox46/itemblacklist/commands/ModCommands.java
+++ src/main/java/github/pitbox46/itemblacklist/commands/ModCommands.java
@@ -3,8 +3,16 @@ package github.pitbox46.itemblacklist.commands;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.tree.LiteralCommandNode;
+import github.pitbox46.itemblacklist.ItemBlacklist;
+import github.pitbox46.itemblacklist.JsonUtils;
+import net.minecraft.Util;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+import net.minecraft.commands.arguments.item.ItemArgument;
+import net.minecraft.network.chat.ChatType;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
 
 public class ModCommands {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
@@ -13,8 +21,22 @@ public class ModCommands {
                         .then(CommandBanItem.register(dispatcher))
                         .then(CommandUnbanItem.register(dispatcher))
                         .then(CommandBanList.register(dispatcher))
+                        .then(Commands.literal("hand")
+                                .executes(context -> hand(context.getSource().getPlayerOrException(), InteractionHand.MAIN_HAND))
+                        )
         );
 
         dispatcher.register(Commands.literal("itemblacklist").redirect(cmdTut));
+    }
+
+    private static int hand(ServerPlayer player, InteractionHand hand) {
+        var stack = player.getItemInHand(hand);
+        var stackcount = stack.getCount();
+
+        JsonUtils.appendItemToJson(ItemBlacklist.BANLIST, player.getItemInHand(hand).getItem());
+        player.sendMessage(new TextComponent("Item banned: ").append(stack.getItem().getRegistryName().toString()), ChatType.CHAT, Util.NIL_UUID);
+        player.getItemInHand(hand).setCount(stackcount);
+
+        return 0;
     }
 }
