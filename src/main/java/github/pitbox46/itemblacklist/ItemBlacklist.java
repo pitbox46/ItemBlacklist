a03e4e22bf34936f28a7943892165ae1cdf72f7e
X_Niter <drumsforlife1999@yahoo.com>
2022-05-22 20:32:24 -0500

    Optimize Imports

--- src/main/java/github/pitbox46/itemblacklist/ItemBlacklist.java
+++ src/main/java/github/pitbox46/itemblacklist/ItemBlacklist.java
@@ -25,7 +25,6 @@ import java.io.File;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
 
 @Mod("itemblacklist")
 public class ItemBlacklist {
