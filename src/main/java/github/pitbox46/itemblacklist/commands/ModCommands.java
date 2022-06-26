96a925e11b042ab71259c16d1e08da33e31f3142
pitbox46 <pitbox46@gmail.com>
2022-06-26 14:33:29 -0400

    Remove recipes for banned items and remove items from inventory on ban command

--- src/main/java/github/pitbox46/itemblacklist/commands/ModCommands.java
+++ src/main/java/github/pitbox46/itemblacklist/commands/ModCommands.java
@@ -2,17 +2,22 @@ package github.pitbox46.itemblacklist.commands;
 
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import github.pitbox46.itemblacklist.ItemBlacklist;
 import github.pitbox46.itemblacklist.JsonUtils;
 import net.minecraft.Util;
+import net.minecraft.commands.CommandSource;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.item.ItemArgument;
 import net.minecraft.network.chat.ChatType;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.players.PlayerList;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.item.ItemStack;
 
 public class ModCommands {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
@@ -22,21 +27,25 @@ public class ModCommands {
                         .then(CommandUnbanItem.register(dispatcher))
                         .then(CommandBanList.register(dispatcher))
                         .then(Commands.literal("hand")
-                                .executes(context -> hand(context.getSource().getPlayerOrException(), InteractionHand.MAIN_HAND))
+                                .executes(ModCommands::hand)
                         )
         );
 
         dispatcher.register(Commands.literal("itemblacklist").redirect(cmdTut));
     }
 
-    private static int hand(ServerPlayer player, InteractionHand hand) {
-        var stack = player.getItemInHand(hand);
-        var stackcount = stack.getCount();
+    private static int hand(CommandContext<CommandSourceStack> context) throws CommandSyntaxException {
+        ItemStack bannedItem = context.getSource().getPlayerOrException().getMainHandItem();
 
-        JsonUtils.appendItemToJson(ItemBlacklist.BANLIST, player.getItemInHand(hand).getItem());
-        player.sendMessage(new TextComponent("Item banned: ").append(stack.getItem().getRegistryName().toString()), ChatType.CHAT, Util.NIL_UUID);
-        player.getItemInHand(hand).setCount(-stackcount);
-
+        JsonUtils.appendItemToJson(ItemBlacklist.BANLIST, bannedItem.getItem());
+        PlayerList playerList = context.getSource().getServer().getPlayerList();
+        playerList.broadcastMessage(new TextComponent("Item banned: ").append(bannedItem.getItem().getRegistryName().toString()), ChatType.CHAT, Util.NIL_UUID);
+        for(ServerPlayer player : playerList.getPlayers()) {
+            for(int i = 0; i < player.getInventory().getContainerSize(); i++) {
+                if(ItemBlacklist.shouldDelete(player.getInventory().getItem(i)))
+                    player.getInventory().getItem(i).setCount(0);
+            }
+        }
         return 0;
     }
 }
---
+++ src/main/java/github/pitbox46/itemblacklist/mixins/RecipeManagerMixin.java
@@ -0,0 +1,32 @@
+package github.pitbox46.itemblacklist.mixins;
+
+import github.pitbox46.itemblacklist.ItemBlacklist;
+import net.minecraft.world.Container;
+import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeManager;
+import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Mixin(RecipeManager.class)
+public class RecipeManagerMixin {
+    @Inject(at = @At(value = "RETURN"), method = "getRecipeFor", cancellable = true)
+    private <C extends Container, T extends Recipe<C>> void onGetRecipe(RecipeType<T> pRecipeType, C pInventory, Level pLevel, CallbackInfoReturnable<Optional<T>> cir) {
+        cir.getReturnValue().ifPresent(value ->
+                cir.setReturnValue(ItemBlacklist.shouldDelete(value.assemble(pInventory)) ? Optional.empty() : Optional.of(value)));
+    }
+
+    @Inject(at = @At(value = "RETURN"), method = "getRecipesFor", cancellable = true)
+    private <C extends Container, T extends Recipe<C>> void onGetRecipes(RecipeType<T> pRecipeType, C pInventory, Level pLevel, CallbackInfoReturnable<List<T>> cir) {
+        cir.setReturnValue(cir.getReturnValue().stream()
+                .filter(entry -> !ItemBlacklist.shouldDelete(entry.assemble(pInventory)))
+                .collect(Collectors.toList()));
+    }
+}
