a45acebb7d675ad92a0e0803d9f87041d9de5de5
pitbox46 <pitbox46@gmail.com>
2022-08-30 22:11:07 -0400

    Fix unbanning of trade items

--- src/main/java/github/pitbox46/itemblacklist/mixins/MerchantMenuMixin.java
+++ src/main/java/github/pitbox46/itemblacklist/mixins/MerchantMenuMixin.java
@@ -1,6 +1,9 @@
 package github.pitbox46.itemblacklist.mixins;
 
 import github.pitbox46.itemblacklist.ItemBlacklist;
+import net.minecraft.Util;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
 import net.minecraft.world.inventory.MerchantMenu;
 import net.minecraft.world.item.trading.MerchantOffers;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,9 +13,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MerchantMenu.class)
 public abstract class MerchantMenuMixin {
-    @Inject(at = @At(value = "RETURN"), method = "getOffers")
+    @Inject(at = @At(value = "RETURN"), method = "getOffers", cancellable = true)
     private void getOffers(CallbackInfoReturnable<MerchantOffers> cir) {
-        if(cir.getReturnValue() != null)
-            cir.getReturnValue().removeIf(offer -> ItemBlacklist.shouldDelete(offer.assemble()));
+        if(cir.getReturnValue() != null) {
+            MerchantOffers returnedOffers = new MerchantOffers(Util.make(new CompoundTag(), tag -> tag.put("Recipes", new ListTag())));
+            cir.getReturnValue().forEach(offer -> {
+                if(!ItemBlacklist.shouldDelete(offer.assemble()))
+                    returnedOffers.add(offer);
+            });
+            cir.setReturnValue(returnedOffers);
+        }
     }
 }
